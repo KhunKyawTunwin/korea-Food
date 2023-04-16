57bc5fcccab75c43eae5514ea8137a2cdeb70cf3
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-04-16 21:02:19 +0630

    SixteenDay Updated

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And Never Miss Latest Updates!</p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your e-mail address" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
 export default Newsletter;
+
+// 4QEQ-WBCJ-BZHW-R4J
+
+// KGFIBFDIFY  small letter
+
+/* 
+
+9762665941
+9449904795
+
+
+
+MEI ID -  6413614419
+Password - Khunmgkyaw@77
+
+
+*/
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
+
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact"></div>
+
+      <div className="app__footer-links_logo"></div>
+
+      <div className="app__footer-links_work"></div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">
+        © {Date()} OMUK Myanmar. All rights reserved.
+      </p>
+    </div>
   </div>
 );
 
